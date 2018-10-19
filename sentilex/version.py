91457088a89ecdec927065c925a1e663e02a0c23
Evgeny A. Stepanov <stepanov.evgeny.a@gmail.com>
2018-10-19 19:48:27 +0200

    adding versioning

--- sentilex/version.py
+++ sentilex/version.py
@@ -3,9 +3,4 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import sentilex.version
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__version__ = sentilex.version.__version__
+__version__ = '0.0.2'
